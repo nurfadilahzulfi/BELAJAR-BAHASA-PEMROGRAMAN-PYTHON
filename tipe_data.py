3ec88b97303b8782a7914d07fce6b5598257658b
nurfadilahzulfi <nurfadilahzulfi@gmail.com>
2024-01-20 02:16:36 +0700

    tambah lagi ya

---
+++ tipe_data.py
@@ -0,0 +1,31 @@
+# tipe data voolean
+print(True)
+
+# tipe data string
+print("Lets study python programming")
+print('Python Programming so Easy')
+
+# tipe data integer
+print(20)
+
+# tipe data float
+print(3.14)
+
+# tipe data Complex
+print(5j)
+
+# tipe data list
+print([1,2,3,4,5])
+print(["satu","dua","tiga"])
+
+# tipe data tuple 
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+# tipe data dictionary
+print({"nama:Budi", 'umur:20'})
+
+# tipe data dictionary dimasukkan ke dalam variabel biodata 
+biodata = {"nama":"Andi", 'umur':21} # proses inisialisasi variabel biodata
+print(biodata) #proses pencetakan variabel biodata yang berisi tipe data dictionary
+print(type(biodata)) #fungsi untuk mencetak jenis tipe data. akan tampil <class 'dict'> yang berarto dict adalah tipe data dictionary
